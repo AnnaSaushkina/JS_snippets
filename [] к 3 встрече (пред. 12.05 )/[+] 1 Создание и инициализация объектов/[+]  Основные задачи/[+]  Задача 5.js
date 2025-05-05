2e8b1e1f9220bde8310cc0b1ca573db075fe73a7
Anna Saushkina <saushkinanna@gmail.com>
2025-05-06 01:05:40 +0300

    выучена 1 тема. решаются задачи из 2 темы

--- [] к 3 встрече (пред. 12.05 )/[+] 1 Создание и инициализация объектов/[+]  Основные задачи/[+]  Задача 5.js
+++ [] к 3 встрече (пред. 12.05 )/[+] 1 Создание и инициализация объектов/[+]  Основные задачи/[+]  Задача 5.js
@@ -5,3 +5,13 @@
 // Ключ theme берется из переменной currentTheme = "dark"
 
 // Ключ version берется из переменной appVersion = "1.0.0"
+
+let currentTheme = "dark"
+let appVersion = "1.0.0"
+
+let config = {
+    theme: currentTheme,
+    version: appVersion
+}
+
+console.log(config)
